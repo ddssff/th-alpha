a7b58231c2a33e2d394877dcc5a35145e1165b6d
Julian K. Arni <jkarni@gmail.com>
2014-05-11 01:19:30 +0200

    Packaging.
            Better haddocks and more sensible exportable functions.

--- tests/tests.hs
+++ tests/tests.hs
@@ -18,22 +18,20 @@ tests = testGroup "Tests" [unitTests]
 unitTests = testGroup "Unit tests"
   [ testCase "Lambda expressions with different bound variables" $
     do
-       b <- areEq [| \x -> x|]  [| \y -> y|]
+       b <- areExpEq [| \x -> x|]  [| \y -> y|]
        assertBool "Expressions not considered equal!" b
   , testCase "Equal literals" $
     do
-       b <- areEq [| 5 |] [| 5 |]
+       b <- areExpEq [| 5 |] [| 5 |]
        assertBool "Expressions not considered equal!" b
   , testCase "Different constructors" $
     do
-       b <- areEq [| Left 5 |]  [| Right 5 |]
+       b <- areExpEq [| Left 5 |]  [| Right 5 |]
        assertBool "Expressions considered equal!" (not b)
   , testCase "Let bindings" $
     do
-       b <- areEq [| let x = 5 in x |] [| let y = 5 in y |]
+       b <- areExpEq [| let x = 5 in x |] [| let y = 5 in y |]
        assertBool "Expressions not considered equal!" b
   ]
-    where areEq e1 e2 = exp_equalM (runQ e1) (runQ e2)
 
-exp_equalM = (join .) . liftM2 exp_equal
 
