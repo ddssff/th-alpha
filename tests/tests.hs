e574b3b27114e1f1baa6595e9e14bf00841785d9
Julian K. Arni <jkarni@gmail.com>
2014-05-11 01:52:30 +0200

    rename areExpEq to areExpAEq

--- tests/tests.hs
+++ tests/tests.hs
@@ -18,19 +18,19 @@ tests = testGroup "Tests" [unitTests]
 unitTests = testGroup "Unit tests"
   [ testCase "Lambda expressions with different bound variables" $
     do
-       b <- areExpEq [| \x -> x|]  [| \y -> y|]
+       b <- areExpAEq [| \x -> x|]  [| \y -> y|]
        assertBool "Expressions not considered equal!" b
   , testCase "Equal literals" $
     do
-       b <- areExpEq [| 5 |] [| 5 |]
+       b <- areExpAEq [| 5 |] [| 5 |]
        assertBool "Expressions not considered equal!" b
   , testCase "Different constructors" $
     do
-       b <- areExpEq [| Left 5 |]  [| Right 5 |]
+       b <- areExpAEq [| Left 5 |]  [| Right 5 |]
        assertBool "Expressions considered equal!" (not b)
   , testCase "Let bindings" $
     do
-       b <- areExpEq [| let x = 5 in x |] [| let y = 5 in y |]
+       b <- areExpAEq [| let x = 5 in x |] [| let y = 5 in y |]
        assertBool "Expressions not considered equal!" b
   ]
 
